4b71af7d8a86a7a959e9c2acb48bbdcaebef1b3c
Ricardo Cuéllar <rcuellarsn@gmail.com>
2025-08-05 16:09:58 -0600

    Devilearn Profile: Add forms views

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,7 +1,38 @@
+from django.urls import reverse_lazy
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import UpdateView
+from .models import Profile
+from .forms import ProfileForm
+from django.contrib import messages
 # Create your views here.
 
 
 def index(request):
     return render(request, "profiles/profile.html")
+
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = Profile
+    form_class = ProfileForm
+    template_name = 'profiles/profile.html'
+    success_url = reverse_lazy('profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user.profile
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        profile = self.get_object()
+        context['profile_picture'] = profile.photo.url if profile.photo else "https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg"
+        return context
+
+    def form_valid(self, form):
+        messages.success(
+            self.request, "Tu perfil se ha actualizado correctamente.")
+        return super().form_valid(form)
